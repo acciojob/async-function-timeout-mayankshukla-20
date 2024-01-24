02a38f59bc04dbcec7f4fd5b83c0d94bd9163141
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2024-01-24 16:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async () => {
+	let text = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+
+	if(text && delay){
+		let promise1 = new Promise((resolve) => {
+		        setTimeout(() => {
+		            resolve(text);
+		        }, delay);
+		    });
+			let data = await promise1;
+		if(data){
+			document.getElementById("output").innerText = data;
+			
+		}
+		
+		}
+})
